9b3956bb6175bb44a32854ad57890a7f2c250f50
Lucie Nadvornik <73028743+lucienadvornik@users.noreply.github.com>
2024-10-11 16:57:05 +0200

    Create terminal_notes.sh

---
+++ terminal_notes.sh
@@ -0,0 +1,24 @@
+#### Terminal notes ####
+
+# Create new virtual environment
+python -m venv myenv
+
+# Activate new virtual environment
+source myenv/bin/activate
+
+# Library installation
+source myenv/bin/activate
+
+#####
+# All installed packages and versions
+conda list
+# pip list
+
+# Info about dependencies
+conda info
+
+# Update
+conda update --all
+
+
+
