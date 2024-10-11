cd7c18faa90e723eef20bbcb08a38a60b55c3a69
Lucie Nadvornik <73028743+lucienadvornik@users.noreply.github.com>
2024-10-11 16:48:51 +0200

    Create jupyter_notes.bash

---
+++ jupyter_notes.bash
@@ -0,0 +1,14 @@
+## JupyterLab
+#Install JupyterLab with pip:
+pip install jupyterlab
+
+#Once installed, launch JupyterLab with:
+jupyter lab
+
+
+## Jupyter Notebook
+#Install the classic Jupyter Notebook with:
+pip install notebook
+
+#To run the notebook:
+jupyter notebook
